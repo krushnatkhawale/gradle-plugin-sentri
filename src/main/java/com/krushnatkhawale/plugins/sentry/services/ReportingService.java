12444fba6e5676efeba7b8dc9cee07539eb72af6
Krushnat <krushnatkhawale@gmail.com>
2024-10-26 18:33:47 +0530

    ProjectInfo: group population - wip

--- src/main/java/com/krushnatkhawale/plugins/sentry/services/ReportingService.java
+++ src/main/java/com/krushnatkhawale/plugins/sentry/services/ReportingService.java
@@ -7,6 +7,7 @@ public class ReportingService {
     public void printReport(SentryReport sentryReport) {
         System.out.println("  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>   S E N T R Y    P L U G I N   <!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         System.out.println("    Application name                : " + ((ProjectInfo)sentryReport.info()).getName());
+        System.out.println("    Group (TBD)                         : " + ((ProjectInfo)sentryReport.info()).getVersion());
         System.out.println("    Version                         : " + ((ProjectInfo)sentryReport.info()).getVersion());
         System.out.println("  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
